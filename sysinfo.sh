322991100c42dd6a03d437e0bc1bdb410a5ccdc2
d3t0xx <seguinotsteven@gmail.com>
2020-04-29 10:22:32 -0700

    updates
    just updates

--- sysinfo.sh
+++ sysinfo.sh
@@ -5,37 +5,28 @@
 # d3toxxx                                   #
 #-------------------------------------------#
 
-echo " "
 echo "-"
 echo OS
 echo "-"
-echo " "
-sleep 1
-uname -n -v
+uname -a
 
 sleep 1
 
-echo " "
 echo "-"
 echo Hostname
 echo "-"
-echo " "
 hostname -s 
 
 sleep 1
 
-echo " "
 echo "-"
 echo Who is logged
 echo "-"
-echo " "
 who -H
 
 sleep 1
 
-echo " "
 echo "-"
 echo "What's my privilege"
 echo "-"
-echo " "
 sudo -l
