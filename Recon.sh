fc269079499c8d362cf9d71ef349c3d7fd0e98fd
d3t0xx <seguinotsteven@gmail.com>
2020-04-30 02:57:48 -0700

    Update Recon.sh

--- Recon.sh
+++ Recon.sh
@@ -6,6 +6,23 @@
 #-------------------------------------------#
 
 ################################################################################
+# Help                                                                         #
+################################################################################
+Help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: scriptTemplate [-g|h|v|V]"
+   echo "options:"
+   echo "g     Print the GPL license notification."
+   echo "h     Print this Help."
+   echo "v     Verbose mode."
+   echo "V     Print software version and exit."
+   echo
+}
+
+################################################################################
 ################################################################################
 # Main program                                                                 #
 ################################################################################
@@ -22,22 +39,6 @@ while getopts ":h" option; do
    esac
 done
 
-################################################################################
-# Help                                                                         #
-################################################################################
-Help()
-{
-   # Display Help
-   echo "Add description of the script functions here."
-   echo
-   echo "Syntax: scriptTemplate [-g|h|v|V]"
-   echo "options:"
-   echo "g     Print the GPL license notification."
-   echo "h     Print this Help."
-   echo "v     Verbose mode."
-   echo "V     Print software version and exit."
-   echo
-}
   
 #NMAP
 echo "                                  "
