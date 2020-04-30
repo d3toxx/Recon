692a30184c3de1879e1a9f1e5fdb289104696975
d3t0xx <seguinotsteven@gmail.com>
2020-04-30 06:59:12 -0700

    Update toolcheck.sh

---
+++ lib/toolcheck.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+declare -r NMAP='which nmap'
+
+
+function tool_not_found() {
+  if [ ! -x $1 ]; then
+    echo "error $1 Not installed"
+    exit 1
+  fi
+  return 1
+}
+
+function not_sudo() {
+if [ $UID -ne 0 -a $EUID -ne 0 ]; then
+  echo "error Gotta SUDO bro"
+  exit 1
+fi
+}
+
+function target_not_set() {
+if [ -z $target ]; then
+  echo "error missing target"
+  usage
+  exit 1
+fi
+}
