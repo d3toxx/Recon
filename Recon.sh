76ab718cf2e16698dc9f09ce437810a7c4da427f
d3t0xx <seguinotsteven@gmail.com>
2020-04-30 03:14:30 -0700

    Update Recon.sh

--- Recon.sh
+++ Recon.sh
@@ -5,68 +5,76 @@
 # d3toxxx                                   #
 #-------------------------------------------#
 
-################################################################################
-# Help                                                                         #
-################################################################################
+#-------------------------------------------#
+# Help                                      #
+#-------------------------------------------#
 Help()
 {
    # Display Help
    echo "Add description of the script functions here."
    echo
-   echo "Syntax: scriptTemplate [-g|h|v|V]"
+   echo "Syntax: scriptTemplate [h|v|o]"
    echo "options:"
-   echo "g     Print the GPL license notification."
    echo "h     Print this Help."
    echo "v     Verbose mode."
-   echo "V     Print software version and exit."
+   echo "o     Save to text file."
    echo
 }
 
-################################################################################
-################################################################################
-# Main program                                                                 #
-################################################################################
-################################################################################
-################################################################################
-# Input Options                                                                #
-################################################################################
+#-------------------------------------------#
+# Main program                              #
+#-------------------------------------------#
+
+#-------------------------------------------#
+# Input Options                             #
+#-------------------------------------------#
 # Get the options
 while getopts ":h" option; do
    case $option in
       h) # display Help
          Help
          exit;;
+         \?) # incorrect option
+         echo "Error: Invalid option"
+         exit;;
    esac
 done
 
+
+NetworkMAP=`nmap -A -T4 $1 -n --open 2>/dev/null \;`
+if [ "$NetworkMAP" ]; then
+  echo -e "\e[00;31m[-] Running NMAP" 
+  echo -e "\n"
+fi
+
   
 #NMAP
-echo "                                  "
-echo "----------------------------------"
-echo NMAP
-echo "----------------------------------"
-echo "                                  "
-sleep 1
-nmap -A -T4 $1 -n --open
+#echo "                                  "
+#echo "----------------------------------"
+#echo NMAP
+#echo "----------------------------------"
+#echo "                                  "
+#sleep 1
+#nmap -A -T4 $1 -n --open
 
-sleep 1
+#sleep 1
 
 #GoBuster
-echo "                                  "
-echo "----------------------------------"
-echo GoBuster
-echo "----------------------------------"
-echo "                                  "
-sleep 1
-gobuster dns -d $1 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 40
+#echo "                                  "
+#echo "----------------------------------"
+#echo GoBuster
+#echo "----------------------------------"
+#echo "                                  "
+#sleep 1
+#gobuster dns -d $1 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 40
 
-sleep 1
+#sleep 1
 
 #Nikto
-echo "                                  "
-echo "----------------------------------"
-echo Nikto
-echo "----------------------------------"
-echo "                                  "
-sleep 1
-nikto -h http://$1/
+#echo "                                  "
+#echo "----------------------------------"
+#echo Nikto
+#echo "----------------------------------"
+#echo "                                  "
+#sleep 1
+#nikto -h http://$1/
